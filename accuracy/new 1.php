59bdf279d3b25bc42d8a8bf65ea124a2e5b14b97
Rayhan-khan-Ridoy <78033774+Rayhan-khan-Ridoy@users.noreply.github.com>
2022-04-05 15:55:29 +0600

    ML

---
+++ accuracy/new 1.php
@@ -0,0 +1,27 @@
+<?php
+class Fruit { 
+  public $name; 
+  public $color; 
+  function __construct($name, $color) 
+  {    $this->name = $name; $this->color = $color;  }
+  function set_name($name){
+    $this->name = $name;    
+  } 
+  function set_color($color){
+    $this->color = $color;    
+  } 
+  function get_name(){
+    return $this->name;    
+  } 
+  function get_color(){
+    return $this->color;
+  } 
+  function __destruct()
+  {    
+  echo "The fruit is {$this->name} and the color is {$this->color}."; 
+  }
+  
+  }
+  
+  $apple = new Fruit("Apple", "red");
+  ?>
